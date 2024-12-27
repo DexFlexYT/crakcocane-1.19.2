839b0d0b0dbd47f75d68a607d6a6dfc3ce0f481c
DexFlex_YT <deximan4ik@gmail.com>
2024-12-27 15:41:14 +0400

    Initial commit

---
+++ src/main/java/org/dexflex/crakcocane/CrakCocaneClient.java
@@ -0,0 +1,10 @@
+package org.dexflex.crakcocane;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrakCocaneClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/org/dexflex/crakcocane/CrakCocaneDataGenerator.java
@@ -0,0 +1,11 @@
+package org.dexflex.crakcocane;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CrakCocaneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
